58337014274fa90b4c86606c1b83a5e82402b3bb
saramohansatyaram <94008359+saramohansatyaram@users.noreply.github.com>
2022-08-18 12:19:54 +0530

    Add files via upload

---
+++ BACKEND/discovery_service/src/test/java/com/aglcropsystem/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aglcropsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
